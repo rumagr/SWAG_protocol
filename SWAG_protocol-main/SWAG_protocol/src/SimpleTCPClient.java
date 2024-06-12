431f971560903d9b87ada57af43ac183a4db0f4f
rumagr <102584385+rumagr@users.noreply.github.com>
2024-06-12 11:36:05 +0200

    Update SimpleTCPClient.java

--- SWAG_protocol-main/SWAG_protocol/src/SimpleTCPClient.java
+++ SWAG_protocol-main/SWAG_protocol/src/SimpleTCPClient.java
@@ -17,34 +17,38 @@ import org.json.*;
 
 public class SimpleTCPClient{
     public static void main(String[] args) throws IOException {
-        String serverAddress = "localhost"; // Die Adresse des Servers
+        String serverAddress = "141.22.27.104"; // Die Adresse des Servers
         int port = 6789; // Der Port, auf dem der Server hört
 
-        try (Socket socket = new Socket(serverAddress, port)) {
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            try (Socket socket = new Socket(serverAddress, port)) {
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-            System.out.println("Enter message");
+                Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
-            String message = myObj.nextLine();  // Read user input
 
-            //String userInput;
-            JSONObject userInput = new JSONObject();
-            userInput.put("1", "2");
-            userInput.put("message", message);
+                while(true) {
+                    System.out.println("Enter message");
+                    String message = myObj.nextLine();  // Read user input
 
-            userInput.write(out);
+                    //String userInput;
+                    JSONObject userInput = new JSONObject();
+                    userInput.put("1", "2");
+                    userInput.put("message", message);
 
-            out.flush();;
+                    userInput.write(out);
+                    out.flush();
+                }
 
-        } catch (UnknownHostException e) {
-            System.err.println("Host unbekannt: " + serverAddress);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("I/O-Fehler beim Verbinden zum Server: " + e.getMessage());
-            System.exit(1);
+
+            } catch (UnknownHostException e) {
+                System.err.println("Host unbekannt: " + serverAddress);
+                System.exit(1);
+            } catch (IOException e) {
+                System.err.println("I/O-Fehler beim Verbinden zum Server: " + e.getMessage());
+                System.exit(1);
+
         }
     }
 }
