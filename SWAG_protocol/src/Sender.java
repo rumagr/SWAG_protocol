4e51c96d77e235a8cdca1c64947f1c40b7a110ae
Bastian Basler <bastian.basler2002@gmail.com>
2024-06-22 16:24:03 +0200

    Komische Sachen, weil IntelliJ noch offen war

--- SWAG_protocol/src/Sender.java
+++ SWAG_protocol/src/Sender.java
@@ -1,8 +1,2 @@
 package src;
 
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-public class Sender {
-    public static Queue<Task> Sender_Queue = new LinkedBlockingQueue<>();
-}
