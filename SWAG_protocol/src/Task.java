6cee75098c8a6d4f89ade6ebc67891660fa34546
Ruben Marin Grez <rumaringr@gmail.com>
2024-06-27 10:12:04 +0200

    Sieht gut aus
    STU ging doch, paket wurde nur nicht mit geloggt

--- SWAG_protocol/src/Task.java
+++ SWAG_protocol/src/Task.java
@@ -130,6 +130,6 @@ public class Task {
     }
 
     public String toString() {
-        return String.format("Task{art=%s, jsonData=%s, user=%s, message=%s, time=%d, id=%s}", art, jsonData, user, message, time, id);
+        return String.format("Task{art=%s, jsonData=%s, jsonPacket=%s, user=%s, message=%s, time=%d, id=%s}", art, jsonData,jsonPacket, user, message, time, id);
     }
 }
